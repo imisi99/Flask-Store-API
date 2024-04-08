af2149d0a5f2eab1f2e3bf7675360b22b90256c3
imisi99 <isongrichard234@gmail.com>
2024-04-08 15:25:07 +0100

    Email error

--- app.py
+++ app.py
@@ -38,11 +38,11 @@ def create_app(db_url= None):
     db_data.init_app(app)
 
     app.config['MAIL_SERVER'] = 'smtp.gmail.com'
-    app.config['MAIL_PORT'] = 587
-    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = "isongrichard234@gmail.com"
     app.config['MAIL_PASSWORD'] = "Imisioluwa234."
-
+    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = True
     mail = Mail(app)
     
     migrate = Migrate(app, db_data)
